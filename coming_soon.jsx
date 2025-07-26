d6e4f1d9dc308a0a06fc97896bb9f54f90457298
Tejus <78625538+TheFlying12@users.noreply.github.com>
2025-07-25 22:23:00 -0400

    Create coming_soon.jsx

---
+++ coming_soon.jsx
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Civic Empowerment!</title>
+  <subtitle>Get involved in your local community!!!</subtitle>
+</head>
+<body>
+  <h1>🚀 Civic Empowerment</h1>
+  <p>Our site is coming soon. Stay tuned!</p>
+</body>
+</html>
